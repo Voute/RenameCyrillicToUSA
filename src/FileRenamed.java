4db965e7f27fd6216c35caf39bbdf29b028bddab
Yegor <Voute@users.noreply.github.com>
2019-07-13 09:01:31 +0400

    uppercase support

--- src/FileRenamed.java
+++ src/FileRenamed.java
@@ -1,53 +1,87 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by egto1016 on 11.07.2019.
  */
 public class FileRenamed extends File{
 
-    static final ArrayList<CharMapping> charMappings = new ArrayList<CharMapping>() {{
-
-        add(new CharMapping("а","a"));
-        add(new CharMapping("б","b"));
-        add(new CharMapping("в","v"));
-        add(new CharMapping("г","g"));
-        add(new CharMapping("д","d"));
-        add(new CharMapping("е","ye"));
-        add(new CharMapping("ж","zsh"));
-        add(new CharMapping("з","z"));
-        add(new CharMapping("и","i"));
-        add(new CharMapping("к","k"));
-        add(new CharMapping("л","l"));
-        add(new CharMapping("м","m"));
-        add(new CharMapping("н","n"));
-        add(new CharMapping("о","o"));
-        add(new CharMapping("п","p"));
-        add(new CharMapping("р","r"));
-        add(new CharMapping("с","s"));
-        add(new CharMapping("т","t"));
-        add(new CharMapping("у","u"));
-        add(new CharMapping("ф","f"));
-        add(new CharMapping("х","h"));
-        add(new CharMapping("ц","ts"));
-        add(new CharMapping("ч","ch"));
-        add(new CharMapping("ш","sh"));
-        add(new CharMapping("щ","sh"));
-        add(new CharMapping("э","e"));
-        add(new CharMapping("ю","yu"));
-        add(new CharMapping("ь","'"));
-        add(new CharMapping("ъ",""));
-        add(new CharMapping("я","ya"));
-
+    static final HashMap<String, String> charMappings = new HashMap<String,String>() {{
+        //lowercase
+        put("а","a");
+        put("б","b");
+        put("в","v");
+        put("г","g");
+        put("д","d");
+        put("е","ye");
+        put("ж","zsh");
+        put("з","z");
+        put("и","i");
+        put("к","k");
+        put("л","l");
+        put("м","m");
+        put("н","n");
+        put("о","o");
+        put("п","p");
+        put("р","r");
+        put("с","s");
+        put("т","t");
+        put("у","u");
+        put("ф","f");
+        put("х","h");
+        put("ц","ts");
+        put("ч","ch");
+        put("ш","sh");
+        put("щ","sh");
+        put("э","e");
+        put("ю","yu");
+        put("ь","'");
+        put("ъ","");
+        put("я","ya");
+        // uppercase
+        put("А","A");
+        put("Б","B");
+        put("В","V");
+        put("Г","G");
+        put("Д","D");
+        put("Е","Ye");
+        put("Ж","Zsh");
+        put("З","Z");
+        put("И","I");
+        put("К","K");
+        put("Л","L");
+        put("М","M");
+        put("Н","N");
+        put("О","O");
+        put("П","P");
+        put("Р","R");
+        put("С","S");
+        put("Т","T");
+        put("У","U");
+        put("Ф","F");
+        put("Х","H");
+        put("Ц","Ts");
+        put("Ч","Ch");
+        put("Ш","Sh");
+        put("Щ","Sh");
+        put("Э","E");
+        put("Ю","Yu");
+        put("Я","Ya");
     }};
 
     File newFile;
 
     FileRenamed(String fileToRename) {
         super(fileToRename);
+        ArrayList<Character> charsToReplace = new ArrayList<>();
         String newFileName = fileToRename;
-        for (CharMapping mapping: charMappings) {
-            newFileName = newFileName.replaceAll(mapping.charToReplace, mapping.replacement);
+
+        for (Character charr : fileToRename.toCharArray()) {
+            if (!charsToReplace.contains(charr)) charsToReplace.add(charr);
+        }
+        for (Character charr: charsToReplace) {
+            newFileName = newFileName.replaceAll(charr.toString(), charMappings.get(charr.toString()));
         }
         newFile = new File(newFileName);
     }
